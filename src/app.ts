9bfa68af484782fadbf5b7c98f1d90886a92a91b
BearingMe <gomes.bruno201@gmail.com>
2022-01-07 11:31:43 -0300

    feat: adiciona uma rota para gerar e outra para ler o token

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,36 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+
+import verifyToken from "./middlewares/token";
 
 const app: express.Application = express();
 const port: any = process.env.PORT ?? 3000;
 
-app.get("/", (req: express.Request, res: express.Response) => {
+app.get("/api", (req: express.Request, res: express.Response) => {
   res.json({
     message: "Hello World",
   });
 });
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+app.post("/api/secret", verifyToken, (req: any, res: express.Response) => {
+  const authData = jwt.verify(req.token, "secretkey");
+
+  res.json({
+    message: "Hello World",
+    authData,
+  });
 });
+
+app.post("/api/login", (req: express.Request, res: express.Response) => {
+  // mock user
+  const user = {
+    username: "admin",
+    password: "admin",
+  };
+
+  const token = jwt.sign({ user }, "secretkey");
+
+  res.json({ token });
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
